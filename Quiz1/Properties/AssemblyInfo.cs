06668679ecaa60c15d7ddf7ee982462d8bd8c461
Antwan Maddox <antwan.maddox@agilethought.com>
2019-07-25 14:44:19 -0400

    commit changes

---
+++ Quiz1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Quiz1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Quiz1")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0c56d919-d345-4408-a6c5-dc8bf7142278")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Quiz1/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Quiz1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        IWebDriver driver;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+
+            //using the . operator to get webdriver properties
+            var pageSource = driver.PageSource;
+            var title = driver.Title;
+            var url = driver.Url;
+
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //clicking on an element
+            driver.Navigate().GoToUrl("http://tailspintoys.azurewebsites.net/");
+            IWebElement modelAirplaneLinkMenu = driver.FindElement(By.LinkText("Model Airplanes"));
+            modelAirplaneLinkMenu.Click();
+
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            //Method for performing a precondition
+
+            //driver object
+            driver = new ChromeDriver();
+
+            //using the '.' operator to view additional methods and properties of the driver object
+            driver.Manage().Window.Maximize();
+            
+
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+    }
+}
